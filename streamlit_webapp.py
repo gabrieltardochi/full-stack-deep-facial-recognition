ad9b21393980747c97e5afe383311c0b6ad72f68
Gabriel Tardochi <ga.tardochisalles@gmail.com>
2023-09-26 12:31:12 -0300

    streamlit app

--- streamlit_webapp.py
+++ streamlit_webapp.py
@@ -3,42 +3,40 @@
 import requests
 import streamlit as st
 
-st.title("Image Processing App")
+st.title("Deep Facial Recognition App :sunglasses:")
+st.text("https://github.com/gabrieltardochi/full-stack-deep-facial-recognition")
+st.subheader("", divider="rainbow")
 
-# Define input fields for image URL, format, and name
+#  input fields for image URL, format, and name
 image_url = st.text_input("Enter Image URL:")
 image_format = st.text_input("Enter Image Format:")
-name = st.text_input("Enter Name:")
+name = st.text_input("Enter Name (only needed if indexing):")
 
-# Create a radio button to choose between processing and recognition
-action = st.radio("Select Action", ["Image Processing", "Image Recognition"])
+# radio button to choose between processing and recognition
+action = st.radio("Select Action", ["Recognize Face", "Index Person's Face"])
 
 if st.button("Submit"):
-    if action == "Image Processing":
-        # Create a dictionary with the input data
-        data = {"image_url": image_url, "image_format": image_format, "name": name}
-
-        # Send a POST request to the FastAPI endpoint for image processing
-        response = requests.post("http://localhost:8080/process_image/", json=data)
-
-        if response.status_code == 200:
-            st.success("Image processed successfully.")
-            st.json(response.json())
-        else:
-            st.error(
-                "Error processing image. Please check your input data and try again."
-            )
-
-    elif action == "Image Recognition":
-        # Send a POST request to the image recognition endpoint
+    if action == "Recognize Face":
         image_recognition_data = {"image_url": image_url, "image_format": image_format}
 
         recognition_response = requests.post(
-            "http://localhost:8080/recognize_image/", json=image_recognition_data
+            "http://localhost:8080/api/v1/recognize", json=image_recognition_data
         )
 
         if recognition_response.status_code == 200:
-            st.success("Image recognized successfully.")
+            st.success("Image recognition ran successfully.")
             st.json(recognition_response.json())
         else:
-            st.error("Error recognizing image.")
+            st.error("Error trying to recognizing image.")
+    elif action == "Index Person's Face":
+        data = {"image_url": image_url, "image_format": image_format, "name": name}
+
+        response = requests.post("http://localhost:8080/api/v1/index", json=data)
+
+        if response.status_code == 200:
+            st.success("Image indexed successfully.")
+            st.json(response.json())
+        else:
+            st.error(
+                "Error indexing image. Please check your input data and try again."
+            )
